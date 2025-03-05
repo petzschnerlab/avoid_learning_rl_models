1ba3f956d58e77e2f303ab509df96a8705359f4b
chadcwilliams <cwilliams.uca@gmail.com>
2025-03-04 16:17:52 -0800

    Updated param name

--- SOMA_RL/helpers/priors.py
+++ SOMA_RL/helpers/priors.py
@@ -38,7 +38,7 @@ def fixed_priors():
             'critic_lr': .48,
             'temperature': 0.06,
             'mixing_factor': 0.7, # From Gold et al., 2012
-            'valence_bias': 0.33,
+            'valence_factor': 0.33,
             'decay_factor': 0.08,
             'novel_value': .50, # Custom
         },
@@ -51,7 +51,7 @@ def fixed_priors():
             'critic_lr': .48,
             'temperature': 0.06,
             'mixing_factor': 0.69,
-            'valence_bias': 0.33, # From Geana et al., 2021:
+            'valence_factor': 0.33, # From Geana et al., 2021:
             'decay_factor': 0.08, # From Geana et al., 2021:
             'novel_value': .50, # Custom
         },
