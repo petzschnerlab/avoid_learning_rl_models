2ac404b739663abe2d8ce94e281b670c55612e01
chadcwilliams <cwilliams.uca@gmail.com>
2025-03-28 11:35:51 -0700

    Updated fit defaults

--- SOMA_RL/SOMA_RL_fit_main.py
+++ SOMA_RL/SOMA_RL_fit_main.py
@@ -17,42 +17,29 @@ if __name__ == "__main__":
     '''
     Supported models: 
         QLearning, ActorCritic
-        Relative, wRelative
+        Relative
         Hybrid2012, Hybrid2021
 
     Standard models:
         QLearning: Standard Q-Learning Model
         ActorCritic: Standard Actor-Critic Model
         Relative: Standard Relative Model (Palminteri et al., 2015)
-        wRelative+bias+decay: Standard Weighted-Relative Model [Proposed model] (Williams et al., in prep)
         Hybrid2012+bias: Standard Hybrid 2012 Model (Gold et al., 2012)
         Hybrid2021+bias+decay: Standard Hybrid 2021 Model (Geana et al., 2021)
 
     Optional Parameters: You can add optional parameters to models by adding them to the model name using a + sign
-        +bias: Adds a valence bias to the model (e.g. wRelative+bias), only usable with wRelative, Hybrid2012, and Hybrid2021
+        +bias: Adds a valence bias to the model (e.g. Hybrid2012+bias), only usable with Hybrid2012, and Hybrid2021
         +novel: Adds a free parameter for the novel stimulus (e.g. QLearning+novel), useable with all models
         +decay: Adds a decay parameter to the model (e.g. QLearning+decay), useable with all models
     '''
 
-    models = ['QLearning', #Standard
-              'QLearning+novel', #Standard + novel
-              
-              'ActorCritic', #Standard
+    models = ['QLearning+novel', #Standard + novel
               'ActorCritic+novel', #Standard + novel
-
-              'Relative', #Standard
               'Relative+novel', #Standard + novel
-
-              'wRelative+bias+decay', #Standard
-              'wRelative+bias+decay+novel', #Standard + novel
-
-              'Hybrid2012+bias', #Standard w/o bias
               'Hybrid2012+bias+novel', #Standard + novel
-
-              'Hybrid2021+bias+decay', #Standard w/o bias
               'Hybrid2021+bias+decay+novel'] #Standard + novel
     
-    training_params = {'training':              'torch',
+    training_params = {'training':              'scipy',
                        'training_epochs':       100,
                        'optimizer_lr':          0.001,
                     }
