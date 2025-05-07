37b12e03634ecd76bd814b5f247f29348dc6a136
chadcwilliams <cwilliams.uca@gmail.com>
2025-05-07 09:07:07 -0700

    Multiprocessing enabled

--- SOMA_RL/SOMA_RL_fit_main.py
+++ SOMA_RL/SOMA_RL_fit_main.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
                   'fixed':                      fixed,
                   'bounds':                     bounds,
                   'number_of_runs':             10,
-                  'multiprocessing':            False,
+                  'multiprocessing':            True,
                   'training':                   'scipy'
                  }
 
